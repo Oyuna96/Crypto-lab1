e33a17d8a7f7a2029d712a4ffd8e07e50f354805
AlexRakhmanov <officialmeloman@mail.ru>
2019-02-17 18:09:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -163,6 +163,67 @@ $(".second-section-button").on('click', function() {
 
 // -----------ТРЕТИЙ ПУНКТ------------------------------------
 
+function encrypt(){
+	var file = document.getElementById("in").files[0];
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		var tfile = new Uint8Array(e.target.result);
+		for(i=0;i<tfile.length;i++){
+			tfile[i] = parseInt(tfile[i]) + parseInt(document.getElementById("shift").value);
+			if(tfile[i]>255){
+				tfile[i]-=256;
+			}
+		}
+	download(tfile);
+  };
+ 	reader.readAsArrayBuffer(file)
+}
 
+function decrypt(){
+	var file = document.getElementById("in").files[0];
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		var tfile = new Uint8Array(e.target.result);
+		for(i=0;i<tfile.length;i++){
+			tfile[i] = parseInt(tfile[i]) - parseInt(document.getElementById("shift").value);
+			if(tfile[i]<0){
+				tfile[i]+=256;
+			}
+		}
+	download(tfile);
+  };
+ 	reader.readAsArrayBuffer(file)
+}
+
+function numberTest(n){
+	if (n>255) {
+		document.getElementById("shift").value=255;
+	}
+	if (n<0) {
+		document.getElementById("shift").value=0;
+	}
+	document.getElementById("hex").value = parseInt(document.getElementById("shift").value).toString(16).toUpperCase();
+}
+
+function download(data){
+	var link = document.createElement('a');
+	bl = new Blob([data]);
+	link.href = URL.createObjectURL(bl);
+		link.download = document.getElementById("in").files[0].name; 
+	var e = new MouseEvent("click");
+	link.dispatchEvent(e);
+}
+
+$(".encode_third").on('click', function(){
+	encrypt();
+});
+
+$(".decode_third").on('click', function(){
+	decrypt();
+});
+
+$("input[type=\"file\"]").on('change', function(){
+	$("#name_of_file").text(document.getElementById('in').files[0].name);
+});
 
 // ----------------------------------------------------------
